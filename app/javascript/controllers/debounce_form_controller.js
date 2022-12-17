88d836b3a42d95775db8638be0a29a3947f54ec5
bindk <sarakenny123@yahoo.com>
2022-12-17 21:50:06 +0700

    debounce search in progress

---
+++ app/javascript/controllers/debounce_form_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="debounce-form"
+export default class extends Controller {
+  static targets = [ "form" ]
+  connect() { console.log("debounce controller connected") }
+  search() {
+    clearTimeout(this.timeout)
+    this.timeout = setTimeout(() => {
+      this.formTarget.requestSubmit()
+    }, 3000)
+  }
+}

cf55b717cef9d672f92ac5bc1086aa91b7d7fd4d
bindk <sarakenny123@yahoo.com>
2023-09-01 10:55:29 +0700

    Refactor route

--- config/routes.rb
+++ config/routes.rb
@@ -1,41 +1,51 @@
 Rails.application.routes.draw do
+  root 'admin/homes#index'
+  namespace :admin do
+    root 'homes#index'
+
+    resources :orders
+    resources :referrers
+    resources :customers
+    resources :products
+  end
 
   scope :home do
-    get 'index', to: 'home#index'
+    # root 'admin/home#index'
+    # get 'index', to: 'home#index'
+  end
 
-    namespace :api do
+  namespace :api do
 
-      post 'add_order', to: 'api#add_order'
-      get 'phone_check', to: 'api#phone_check'
-      get 'ref_phone_check', to: 'api#ref_phone_check'
-      get 'product_info', to: 'api#product_info'
-      post 'customer_check', to: 'api#customer_check'
-      get 'customer_address', to: 'api#customer_address'
-      get 'cus_name_search', to: 'api#cus_name_search'
-      get 'ref_name_search', to: 'api#ref_name_search'
+    post 'add_order', to: 'api#add_order'
+    get 'phone_check', to: 'api#phone_check'
+    get 'ref_phone_check', to: 'api#ref_phone_check'
+    get 'product_info', to: 'api#product_info'
+    post 'customer_check', to: 'api#customer_check'
+    get 'customer_address', to: 'api#customer_address'
+    get 'cus_name_search', to: 'api#cus_name_search'
+    get 'ref_name_search', to: 'api#ref_name_search'
 
 
-      # post 'add_prod', to: 'api#add_prod'
-      # get 'find_prod', to: 'api#find_prod'
-      # put 'update_prod', to: 'api#update_prod'
-      # delete 'delete_prod', to: 'api#delete_prod'
-      # get 'prod_page', to: 'api#prod_page'
-      resources :products
+    # post 'add_prod', to: 'api#add_prod'
+    # get 'find_prod', to: 'api#find_prod'
+    # put 'update_prod', to: 'api#update_prod'
+    # delete 'delete_prod', to: 'api#delete_prod'
+    # get 'prod_page', to: 'api#prod_page'
+    resources :products
 
-      namespace :orders do
-        get 'find_order', to: 'find_order'
-        get 'find_oder_kw', to: 'find_oder_kw'
-        put 'change_order_stat', to: 'change_order_stat'
-        get 'find_order_by_stat', to: 'find_order_by_stat'
-        get 'order_page', to: 'order_page'
-      end
-
-      get 'cus_search', to: 'api#cus_search'
-      get 'ref_search', to: 'api#ref_search'
-      put 'cus_info', to: 'api#cus_info_update'
-      put 'cus_addr', to: 'api#cus_addr_update'
-      put 'ref_info', to: 'api#ref_info_update'
+    namespace :orders do
+      get 'find_order', to: 'find_order'
+      get 'find_oder_kw', to: 'find_oder_kw'
+      put 'change_order_stat', to: 'change_order_stat'
+      get 'find_order_by_stat', to: 'find_order_by_stat'
+      get 'order_page', to: 'order_page'
     end
+
+    get 'cus_search', to: 'api#cus_search'
+    get 'ref_search', to: 'api#ref_search'
+    put 'cus_info', to: 'api#cus_info_update'
+    put 'cus_addr', to: 'api#cus_addr_update'
+    put 'ref_info', to: 'api#ref_info_update'
   end
 
   scope :orders do
@@ -49,7 +59,7 @@ Rails.application.routes.draw do
     get 'product_manage', to: 'home#product_manage'
     get 'new', to: 'home#new_product'
     post 'save_product', to: 'home#save_product'
-    get 'product_detail', to: 'home#product_detail'
+    # get 'product_detail', to: 'home#product_detail'
     put 'update_product', to: 'home#update_product'
   end
 
@@ -69,9 +79,6 @@ Rails.application.routes.draw do
     put 'update_customer_addr', to: 'home#update_customer_addr'
 
   end
-
-  root 'home#index'
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
